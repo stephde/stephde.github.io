7a392aa7793d00df3f44e92104401b376cc18d2e
Stephan Detje <stephan.detje@sap.com>
2015-12-30 18:14:39 -0800

    add facivon and optimize css delivery

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,14 @@
+function loadStyleSheet(src){
+	if (document.createStyleSheet) document.createStyleSheet(src);
+	else {
+		var stylesheet = document.createElement('link');
+		stylesheet.href = src;
+		stylesheet.rel = 'stylesheet';
+		stylesheet.type = 'text/css';
+		document.getElementsByTagName('head')[0].appendChild(stylesheet);
+	}
+}
+
 function goToByScroll(id){
     $('#content').animate({scrollTop: $("#"+id).offset().top},'slow')
 
